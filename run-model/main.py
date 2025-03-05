e34c3c2c4e9487d5797e5f78c11945f7bfa11f7b
Steve Rosam <steve+newdemo@quix.io>
2025-03-05 19:56:17 +0000

    Edited 'main.py'

--- run-model/main.py
+++ run-model/main.py
@@ -5,6 +5,7 @@ import json
 from sklearn.linear_model import LinearRegression
 from quixstreams import Application
 
+
 # Connect to Redis
 r = redis.StrictRedis(host='redis', port=6379, db=0, decode_responses=True)
 
